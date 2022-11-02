64cbd3b7c63ce951337cb3846d6102dca2ddcb68
WizardGote <InvaGirSP@gmail.com>
2022-11-02 05:29:05 +0200

    Update sh_plugin.lua

--- usergroupshandler/sh_plugin.lua
+++ usergroupshandler/sh_plugin.lua
@@ -2,10 +2,11 @@ local PLUGIN = PLUGIN
 
 PLUGIN.name = "Usergroups Handler"
 PLUGIN.author = "Ceryx"
-PLUGIN.description = "Handles usergroups by SteamID"
+PLUGIN.description = "Handles usergroups by SteamID on player spawn"
 
 PLUGIN.Users = {
-	["STEAM_0:1:31168128"] = "superadmin",
+	--["SteamID_Example"] = "admin",
+	--["SteamID_Example2"] = "superadmin"
 }
 
 ix.util.Include("sv_plugin.lua")
